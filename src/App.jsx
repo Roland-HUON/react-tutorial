4405282c46b4f5c6bcf2b9fbf30f9a782a2427ea
Roland HUON <huonroland91@gmail.com>
2024-06-04 10:33:38 +0200

    Refactor App component to use React Fragment and include HTML markup for better structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { Fragment } from "react"
 
+const title = '<strong>bonjour</strong>'
+const style ={color: 'red', backgroundColor: 'blue'}
+const showtitle = true
+
+
 function App() {
+
+  const handleClick = (e) =>{
+    console.log(e);
+    alert ("j'ai cliqué sur le h1");
+  }
+
   return <Fragment>
-    <h1 id="title" className="title">Bonjour</h1>
+    {showtitle &&<h1 onClick={handleClick} id="title" className="title" dangerouslySetInnerHTML={{__html:title}} style={style}></h1>}
+    <input type="text" />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corporis molestiae obcaecati! Iste, quibusdam suscipit similique officiis odio sint minus id architecto veniam eaque illum voluptate dolorum molestiae placeat. Provident!
     Neque maiores facilis cumque minus rerum harum at fugiat. Obcaecati, consectetur delectus! Tempore animi expedita exercitationem tenetur est dolorum quam asperiores molestiae blanditiis, iusto, incidunt voluptates accusantium sapiente quisquam quasi.
     Illo a iusto provident dolores, aliquid debitis, fugiat itaque esse unde maxime eaque saepe quisquam dicta odio? Cumque laudantium voluptatibus vitae architecto, laborum possimus obcaecati, illum aut beatae sed facere.
