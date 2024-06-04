9f1bada925ee1781e4f2d93f53018351fc83657f
Roland HUON <huonroland91@gmail.com>
2024-06-04 10:11:59 +0200

    chore: Refactor App component to use React Fragment for better structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { Fragment } from "react"
+
 function App() {
-  return <div>
+  return <Fragment>
     <h1 id="title" className="title">Bonjour</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corporis molestiae obcaecati! Iste, quibusdam suscipit similique officiis odio sint minus id architecto veniam eaque illum voluptate dolorum molestiae placeat. Provident!
     Neque maiores facilis cumque minus rerum harum at fugiat. Obcaecati, consectetur delectus! Tempore animi expedita exercitationem tenetur est dolorum quam asperiores molestiae blanditiis, iusto, incidunt voluptates accusantium sapiente quisquam quasi.
     Illo a iusto provident dolores, aliquid debitis, fugiat itaque esse unde maxime eaque saepe quisquam dicta odio? Cumque laudantium voluptatibus vitae architecto, laborum possimus obcaecati, illum aut beatae sed facere.
     Mollitia sit autem distinctio, iusto omnis, quae aut atque quas quasi accusantium illum tenetur esse alias necessitatibus porro sed nobis ab culpa iste nemo. Doloremque quod fugit error sunt atque?
     Atque sunt dolore amet voluptates sit quam veritatis iure et perspiciatis rem sequi cumque quos ipsam cum, non consequatur itaque harum mollitia obcaecati delectus est ullam. Quasi officiis ut doloribus?</p>
-  </div>
+  </Fragment>
 }
 
 export default App
