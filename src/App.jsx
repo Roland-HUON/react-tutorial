1c90680f770d591e45bf579675c5f1ab3ba95e48
Roland HUON <huonroland91@gmail.com>
2024-06-05 10:33:34 +0200

    Refactor App component to use React Fragment and include HTML markup for better structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,21 @@ import { Fragment } from "react"
 import { useState } from "react"
 
 function App() {
-  const [Person, setPerson] = useState({
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 18
-  })
-  const [count, setCount] = useState(0)
-  // console.log('render')
-  const incrementAge = () =>{
-    setPerson({...Person, age: Person.age + 1})
+
+  const [firstname, setFirstname] = useState('John Doe')
+
+  const handleChange = (e) => {
+    setFirstname(e.target.value)
   }
-  const incrementCount = () =>{
-    setCount(count + 1)
+
+  const reset = () => {
+    setFirstname('')
   }
+
   return <Fragment>
-    <p>Age de {Person.firstName} : {Person.age}</p>
-    <button onClick={incrementAge}>Gagner une année</button>
-    <button onClick={incrementCount}>Incrémenter : {count}</button>
-    </Fragment>
+    <input type="text" name="firstname" value={firstname} onChange={handleChange}/>  
+    <button onClick={reset} type="button">Reset</button>
+  </Fragment>
 }
 
 export default App
